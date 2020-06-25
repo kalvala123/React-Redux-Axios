b1b3ce9537b310d836879ad47bba54943eea4c11
kalvala123 <31452487+kalvala123@users.noreply.github.com>
2020-06-24 23:41:50 -0400

    react redux

--- src/reducers/addItemReducer.js
+++ src/reducers/addItemReducer.js
@@ -2,13 +2,17 @@
 * Reducers are pure functions that take previous state and action and return new state
 * and tells the store "how to do"
 */
-let initialState = {
+const initialState = {
     items:[]
 }
 const addItemReducer = (state = initialState, action) => {
+    alert(state.items.length)
     switch (action.type) {
         case "ADD_ITEM":
-            return state.items.concat(action.payload)
+            return {
+                ...state,
+                items:state.items.concat(action.payload)
+            }
         default:
             return state
     }
